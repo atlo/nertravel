6b696d7aa293f08dfd4892e33b3734eac9392eb9
László Horváth <bedafoltyn@gmail.com>
2018-11-06 22:36:06 +0100

    selects are separated, real data added

--- src/select.js
+++ src/select.js
@@ -1,26 +1,23 @@
 import Choices from 'choices.js'
 
-const selectButton = document.querySelector('.date-select')
-const select = document.querySelector('#choices-single-remote-fetch')
+const planeSelect = document.querySelector('#plane-choices-single-remote-fetch')
+const yachtSelect = document.querySelector('#yacht-choices-single-remote-fetch')
 
-const planePlace = document.querySelector('.plane .place')
-const yachtPlace = document.querySelector('.yacht .place')
-const yachtDate = document.querySelector('.yacht .date')
-
-new Choices(select, {searchEnabled: false})
-
-function updatePlace () {
-  const {value, text} = select[select.selectedIndex]
-
-  planePlace.innerHTML = value
-  yachtPlace.innerHTML = value
-  yachtDate.innerHTML = text
+const options = {
+  shouldSort: false,
+  searchEnabled: false
 }
 
-selectButton.addEventListener('click', function () {
-  document.querySelector('.choices__list--dropdown').classList.toggle('is-active')
-})
+new Choices(planeSelect, options)
+new Choices(yachtSelect, options)
 
-select.addEventListener('change', updatePlace)
+function updatePlace (event) {
+  const element = event.target
+  const {value} = element[element.selectedIndex]
+  const place = element.closest('.vehicle').querySelector('.place')
 
-updatePlace()
+  place.innerHTML = value
+}
+
+planeSelect.addEventListener('change', updatePlace)
+yachtSelect.addEventListener('change', updatePlace)

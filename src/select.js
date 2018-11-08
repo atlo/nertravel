1e4611989b6c797bcfe17f97c54dead6c92cfaf0
László Horváth <bedafoltyn@gmail.com>
2018-11-08 09:58:12 +0100

    select refactored, setCurrent and updatePlace fixed

--- src/select.js
+++ src/select.js
@@ -4,19 +4,17 @@ import {format} from 'date-fns'
 const planeSelect = document.querySelector('#plane-choices-single-remote-fetch')
 const yachtSelect = document.querySelector('#yacht-choices-single-remote-fetch')
 const planeSelectOptions = Array.from(planeSelect.querySelectorAll('option'))
-const yachtSelectOptions = Array.from(planeSelect.querySelectorAll('option'))
+const yachtSelectOptions = Array.from(yachtSelect.querySelectorAll('option'))
 
-function updatePlace (event) {
-  const element = event.target
-  const {value} = element[element.selectedIndex]
-  const place = element.closest('.vehicle').querySelector('.place')
+function updatePlace (select) {
+  const {value} = select[select.selectedIndex]
+  const place = select.closest('.vehicle').querySelector('.place')
 
   place.innerHTML = value
 }
 
 function setCurrentLocation (select, firstOption) {
- const today = format(new Date(), 'YYYY. MM. DD.')
-
+ const today = format(new Date(), 'YYYY.MM.DD.')
   if (firstOption.text !== today) {
     const newOption = document.createElement('option')
     newOption.text = today
@@ -24,16 +22,18 @@ function setCurrentLocation (select, firstOption) {
     newOption.selected = true
 
     select.prepend(newOption)
-  } 
+  }
 
-  new Choices(select, {
-    shouldSort: false,
-    searchEnabled: false
-  })
+  updatePlace(select)
 }
 
 setCurrentLocation(planeSelect, planeSelectOptions[0])
 setCurrentLocation(yachtSelect, yachtSelectOptions[0])
 
-planeSelect.addEventListener('change', updatePlace)
-yachtSelect.addEventListener('change', updatePlace)
+new Choices('select', {
+  shouldSort: false,
+  searchEnabled: false
+})
+
+planeSelect.addEventListener('change', event => updatePlace(event.target))
+yachtSelect.addEventListener('change', event => updatePlace(event.target))

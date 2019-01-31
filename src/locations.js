3a011d1161707e7d902ef973ac7e3a8c42e33d74
László Horváth <bedafoltyn@gmail.com>
2019-01-31 09:46:02 +0100

    location year fixed

--- src/locations.js
+++ src/locations.js
@@ -1173,7 +1173,7 @@ export const plane2Locations = [
     longitude: '19.040235,24.5625,19.040235'
   },
   {
-    date: '2018.01.30.',
+    date: '2019.01.30.',
     location: 'Budapest,Szófia,Budapest',
     latitude: '47.497912,42.6977082,47.497912',
     longitude: '19.040235,23.3218675,19.040235'
